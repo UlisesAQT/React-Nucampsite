f37b4c36c738d2f4d4c084a649c4cb755f019a7c
UlisesT <ulisestamayo1998@gmail.com>
2025-11-23 22:17:52 -0500

    created 2 columns for the campsites and added a random campsite picker, fixed bugs

--- src/App.js
+++ src/App.js
@@ -1,21 +1,15 @@
-import React from 'react';
-import {Container, Navbar,NavbarBrand} from 'reactstrap';
-import NucampLogo from './app/assets/img/logo.png';
-import logo from './logo.svg';
-
+import Header from './components/Header';
+import CampsitesDirectoryPage from './pages/CampsitesDirectoryPage';
+import Footer from './components/Footer';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-        <Navbar dark color='primary' sticky='top' expand='md'>
-            <Container>
-                <NavbarBrand href='/'>
-                  <img src={NucampLogo} alt='nucamp logo'/>
-                </NavbarBrand>
-            </Container>
-        </Navbar>
-        Ready for workshop 
+        <Header/>
+        <CampsitesDirectoryPage />
+        <Footer/>
     </div>
   );
 }
---
+++ src/features/campsites/CampsitesList.js
@@ -0,0 +1,69 @@
+
+import { Row, Col } from 'reactstrap';
+import CampsiteCard from './CampsiteCard';
+import { selectAllCampsites } from './campSlice';
+
+
+const CampsitesList = () => {
+    const campsites = selectAllCampsites();
+
+    return (
+        <Row className="ms-auto">
+            {campsites.map((campsite) => (
+                <Col md={5} className="m-4" key={campsite.id}>
+                    <CampsiteCard campsite={campsite} />
+                </Col>
+            ))}
+        </Row>
+    );
+}
+
+export default CampsitesList;
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import {Col, Row } from 'reactstrap';
+// import CampsiteCard from "./CampsiteCard";
+// import { selectAllCampsites } from './campsitesSlice';
+// import { useSelector } from 'react-redux';
+
+
+
+
+// const CampsitesList = () => {
+//     const campsites = selectAllCampsites();
+
+//     return(
+//         const CampsitesList = () => {
+//     const campsites = useSelector(selectAllCampsites); // <-- useSelector is required
+
+//     return (
+//         // <Row className='ms-auto'>
+//         //     {campsites.map((campsite) => {
+//         //         return(
+//         //         <Col md='5' className='m-4' key={campsite.id}> 
+//         //         <CampsiteCard campsite={campsite}/> 
+//         //         </Col>
+//         //         );
+//         //     })}
+//         // </Row>
+       
+//     );
+// }
+
+        
+//     );
+// }
+// export default CampsitesList;
+
+
