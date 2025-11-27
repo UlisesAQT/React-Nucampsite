970747f095769536f9ff32a8e5dfa447e93a850f
UlisesT <ulisestamayo1998@gmail.com>
2025-11-27 16:49:10 -0500

    added featured campsite feature for home page and updated navbar

--- src/features/campsites/campSlice.js
+++ src/features/campsites/campSlice.js
@@ -11,3 +11,6 @@ import {CAMPSITES } from '../../app/shared/CAMPSITES';
 export const selectCampsiteById = (id) =>{
     return CAMPSITES.find((campsite) => campsite.id === id); 
 }
+export const selectFeaturedCampsite = () => {
+    return CAMPSITES.find((campsite) => campsite.featured);
+}
