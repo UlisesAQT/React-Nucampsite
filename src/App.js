9fba320d0ea1ee650e3c8892f9f16b9d75a69464
UlisesT <ulisestamayo1998@gmail.com>
2025-11-27 13:00:41 -0500

    added React Routes to homepage, Contact and Directory

--- src/App.js
+++ src/App.js
@@ -3,13 +3,20 @@ import CampsitesDirectoryPage from './pages/CampsitesDirectoryPage';
 import Footer from './components/Footer';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import ContactPage from './pages/ContactPage';
 
 
 function App() {
   return (
     <div className="App">
         <Header/>
-        <CampsitesDirectoryPage />
+        <Routes>
+            <Route path='/' element={<HomePage/>} />
+            <Route path='contact' element={<ContactPage/>} />
+            <Route path='directory' element={<CampsitesDirectoryPage/>} />
+        </Routes>
         <Footer/>
     </div>
   );
