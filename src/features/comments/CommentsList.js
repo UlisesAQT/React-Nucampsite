6ff8bfbe9917aab69854eb4e8f7e87f67dec47e7
UlisesT <ulisestamayo1998@gmail.com>
2025-12-06 16:03:54 -0500

    added comment forms and added error messages

--- src/features/comments/CommentsList.js
+++ src/features/comments/CommentsList.js
@@ -1,7 +1,7 @@
 import { Col } from 'reactstrap';
 import Comment from './Comment';
 import { selectCommentsByCampsiteId } from './commentsSlice';
-
+import CommentForm from './CommentForm';
 
 
 const CommentsList = ({ campsiteId }) => {
@@ -14,6 +14,7 @@ const CommentsList = ({ campsiteId }) => {
                 {comments.map((comment) => {
                     return <Comment key={comment.id} comment={comment} />;
                 })}
+                <CommentForm campsiteId={campsiteId}/>
             </Col>
         );
     }
