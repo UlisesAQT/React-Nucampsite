7e43b3b42a133238a6704e690b899e3e473a629e
UlisesT <ulisestamayo1998@gmail.com>
2025-12-10 14:07:44 -0500

    redux for all

--- src/features/comments/commentsSlice.js
+++ src/features/comments/commentsSlice.js
@@ -1,6 +1,19 @@
 import { COMMENTS } from '../../app/shared/COMMENTS';
-export const selectCommentsByCampsiteId = (campsiteId) => {
-    return COMMENTS.filter(
-        (comment) => comment.campsiteId === parseInt(campsiteId)
-    );
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+    commentsArray: COMMENTS
+};
+
+const commentsSlice = createSlice({
+    name: 'comments',
+    initialState
+});
+
+export const commentsReducer = commentsSlice.reducer
+
+export const selectCommentsByCampsiteId = (campsiteId) => (state) => {
+    return state.comments.commentsArray.filter((comment) => comment.campsiteId === parseInt(campsiteId)
+);
+    
 };
